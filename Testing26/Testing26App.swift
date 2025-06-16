81b4455745cb886abe88a4888bd4814619290de6
Ivan Terziev <ivanterziev38@gmail.com>
2025-06-16 22:57:09 +0300

    Initial Commit

---
+++ Testing26/Testing26App.swift
@@ -0,0 +1,17 @@
+//
+//  Testing26App.swift
+//  Testing26
+//
+//  Created by Ivan Terziev on 6/16/25.
+//
+
+import SwiftUI
+
+@main
+struct Testing26App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
