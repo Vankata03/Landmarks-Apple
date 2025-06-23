7c9eb6f01ca2ccfce3ed1abe538126a9c718e2b9
Ivan Terziev <ivanterziev38@gmail.com>
2025-06-24 00:06:55 +0300

    Finnish working on the UIKit components

--- Testing26/Views/PageView/PageControl.swift
+++ Testing26/Views/PageView/PageControl.swift
@@ -8,13 +8,22 @@
 import SwiftUI
 import UIKit
 
-struct PageController: UIViewRepresentable {
+struct PageControl: UIViewRepresentable {
     var numberOfPages: Int
     @Binding var currentPage: Int
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
     
     func makeUIView(context: Context) -> UIPageControl {
         let control = UIPageControl()
         control.numberOfPages = numberOfPages
+        control.addTarget(
+            context.coordinator,
+            action: #selector(Coordinator.updateCurrentPage(_:)),
+            for: .valueChanged
+        )
         
         return control
     }
@@ -22,5 +31,17 @@ struct PageController: UIViewRepresentable {
     func updateUIView(_ uiView: UIPageControl, context: Context) {
         uiView.currentPage = currentPage
     }
-    
+        
+    class Coordinator: NSObject {
+        var control: PageControl
+        
+        init(_ control: PageControl) {
+            self.control = control
+        }
+        
+        @objc
+        func updateCurrentPage(_ sender: UIPageControl) {
+            control.currentPage = sender.currentPage
+        }
+    }
 }
